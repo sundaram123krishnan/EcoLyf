cf314fde64b834f6334457d8f41f6d1a11163784
sundaram123krishnan <krishnsundaram@gmail.com>
2024-11-09 15:29:36 +0530

    activity template

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
 
+    import * as Select from "$lib/components/ui/select/index.js";
+
 
 </script>
 
+<h1>Add Activity</h1>
+
+<Select.Root type="single">
+    <Select.Trigger class="w-[180px]"></Select.Trigger>
+    <Select.Content>
+      <Select.Item value="private vehicles">Private Vehicles</Select.Item>
+      <Select.Item value="waste disposal">Waste Disposal</Select.Item>
+      <Select.Item value="plastics usage">Plastics Usage</Select.Item>
+      <Select.Item value="water usage">Excessive Water Usage</Select.Item>
+      <Select.Item value="smoking indoors">Smoking Indoors</Select.Item>
+      <Select.Item value="using aerosols">Using Aerosols</Select.Item>
+    </Select.Content>
+  </Select.Root>
 
