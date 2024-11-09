45201211e141ee41aa9c6bc2c333ec7f8bb77367
sundaram123krishnan <krishnsundaram@gmail.com>
2024-11-09 15:14:25 +0530

    page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,7 @@
 	import { ModeWatcher } from 'mode-watcher';
 	let { children } = $props();
 	import SideBar from './SideBar.svelte';
-	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 </script>
 
-<Sidebar.Provider>
-	<SideBar />
-	<main>
-		<Sidebar.Trigger />
-		{@render children?.()}
-	</main>
-</Sidebar.Provider>
+{@render children?.()}
