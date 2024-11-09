a402146a82b934f972c8e10d44b4461099554b79
WhyAsh5114 <whyash5114@gmail.com>
2024-11-09 18:13:55 +0530

    add breadcrumb

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,30 @@
 	let { children } = $props();
 	import SideBar from './SideBar.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
 </script>
 
 <Sidebar.Provider>
 	<SideBar />
 	<main>
-		<Sidebar.Trigger />
+		<div class="flex items-center gap-2">
+			<Sidebar.Trigger />
+			<Breadcrumb.Root>
+				<Breadcrumb.List>
+					<Breadcrumb.Item>
+						<Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+					</Breadcrumb.Item>
+					<Breadcrumb.Separator />
+					<Breadcrumb.Item>
+						<Breadcrumb.Link href="/components">Components</Breadcrumb.Link>
+					</Breadcrumb.Item>
+					<Breadcrumb.Separator />
+					<Breadcrumb.Item>
+						<Breadcrumb.Page>Breadcrumb</Breadcrumb.Page>
+					</Breadcrumb.Item>
+				</Breadcrumb.List>
+			</Breadcrumb.Root>
+		</div>
 		{@render children?.()}
 	</main>
 </Sidebar.Provider>
